7f1bb710a7be4e162fabab7cf6f358c7a7a02b0d
Eric Gjerde <gjerdeeric@gmail.com>
2022-02-03 00:25:19 -0600

    Update

--- Anagram/Anagram.hs
+++ Anagram/Anagram.hs
@@ -6,7 +6,10 @@ type Bucket = [Int]
 type BucketMap = Dm.Map Char Bucket
 type CountMap = Dm.Map Char Int
 
-main = print (countMap $ analyze "aaaaa")
+main = do
+    print (drop 3 "abcde")
+    print (take 3 "abcde")
+
 --main = print (analyzeAnagrams "aaaaa" "aaa")
 
 between :: Int -> Int -> (Int -> Bool)
@@ -21,20 +24,22 @@ analyze s = Dm.fromList $ map (\letter -> (letter, elemIndices letter s)) (nub $
 countMap :: BucketMap -> CountMap
 countMap = Dm.map length
 
-compareMap :: BucketMap -> (BucketMap -> Bool)
+compareMap :: CountMap -> (CountMap -> Bool)
 compareMap mapA mapB =
-    DM.foldrWithKey (&&) (\k x _ -> )
-    and (zipWith (==) (countMap mapA) (countMap mapB))
+    all (\letter -> Dm.lookup letter mapA == Dm.lookup letter mapB) (Dm.keys mapA)
+
+compareBucket :: Bucket -> (Bucket -> Bool)
+compareBucket bA bB = length bA == length bB
 
 analyzeAnagrams :: String -> String -> [Int]
 analyzeAnagrams source target = searchAnagrams source target [] 0
 
 searchAnagram :: String -> String -> Int -> Bool
-searchAnagram source target i = compareMap (analyze (drop i source)) (analyze target)
+searchAnagram source target i = compareMap (countMap $ analyze (drop i source)) (countMap $ analyze target)
 
 searchAnagrams :: String -> String -> [Int] -> Int -> [Int]
-searchAnagrams source target x i | i >= (length source - length target) = x
-searchAnagrams source target x i | i < (length source - length target) =
+searchAnagrams source target x i | i >= length source - length target = x
+searchAnagrams source target x i | i < length source - length target =
     if searchAnagram source target i
-        then searchAnagrams source target (x ++ [i]) (i + 1)
-    else searchAnagrams source target x (i + 1)
+        then x ++ searchAnagrams source target [i] (i + 1)
+    else x ++ searchAnagrams source target [] (i + 1)
